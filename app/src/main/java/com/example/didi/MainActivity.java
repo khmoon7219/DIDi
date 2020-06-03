60cc2a3279acaedeaa9a80019488f6779a9d56e1
khmoon7219 <khmoon7219@gmail.com>
2020-06-03 16:15:24 +0900

    initial commit

--- app/src/main/java/com/example/didi/MainActivity.java
+++ app/src/main/java/com/example/didi/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.didi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button customButton = (Button) findViewById(R.id.custom_button);
+        Switch switchEnableButton = (Switch) findViewById(R.id.switch_enable_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switchEnableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    customButton.setEnabled(true);
+                } else {
+                    customButton.setEnabled(false);
+                }
+            }
+        });
+
     }
 }
